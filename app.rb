c17a4f171a62d22b2ee3c70d0d95853fb86f40ab
Ryan Kohler <ryank@westwardleaning.com>
2017-04-10 17:35:46 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,2 +1,2 @@
-letters = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "peter"]
+letters = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa"]
 puts letters.join(", ")
