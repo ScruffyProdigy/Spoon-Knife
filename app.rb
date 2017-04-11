ab385991f4f2c9d56fb05fb04e9056567d452a52
Ryan Kohler <ryank@westwardleaning.com>
2017-04-10 18:18:59 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 letters = [
-  "alpha", "bravo", "charlie", "delta",
-  "echo", "foxtrot", "golf", "hotel",
-  "india", "juliet", "kilo", "lima",
-  "mike", "november", "oscar", "papa", 
-  "quebec", "romeo", "sierra", "tango",
-  "umbrella", "victor", "whiskey", "x-ray",
-  "yankee", "zulu", "?????", "And, per-se, and", "!!!!!!!", 
+  "alpha", "bravo", "charlie", "delta", "echo", 
+  "foxtrot", "golf", "hotel", "india", "juliet", 
+  "kilo", "lima", "mike", "november", "oscar", 
+  "papa", "quebec", "romeo", "sierra", "tango",
+  "umbrella", "victor", "whiskey", "x-ray", "yankee", 
+  "zulu", "?????", "And, per-se, and", "!!!!!!!", 
  ]
 
 puts letters.join(", ")
