6be9ea21021d07203da4c3ad08e8814a4acca6fe
Ryan Kohler <ryank@westwardleaning.com>
2017-04-12 15:55:29 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 letters = [
   "A: alpha", "B: bravo", "C: charlie", "D: delta", "E: echo", 
   "F: foxtrot", "G: golf", "H: hotel", "I: india", "J: juliet", 
-  "K: kilo", "lima", "mike", "november", "oscar", 
+  "K: kilo", "L:lima", "mike", "november", "oscar", 
   "papa", "quebec", "romeo", "sierra", "tango",
   "umbrella", "victor", "whiskey", "x-ray", "yankee", 
   "zulu", "?????", "And, per-se, and", "!!!!!!!", "whatever!", "adding garbage!!!!!",  "So Annoying", 
