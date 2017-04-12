37420a2dbe16eaa9b1f93c543932d0f692ea4f6b
Ryan Kohler <ryank@westwardleaning.com>
2017-04-12 14:58:53 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 letters = [
   "A: alpha", "B: bravo", "C: charlie", "D: delta", "E: echo", 
-  "F: foxtrot", "G: golf", "H: hotel", "india", "juliet", 
+  "F: foxtrot", "G: golf", "H: hotel", "I: india", "juliet", 
   "kilo", "lima", "mike", "november", "oscar", 
   "papa", "quebec", "romeo", "sierra", "tango",
   "umbrella", "victor", "whiskey", "x-ray", "yankee", 
