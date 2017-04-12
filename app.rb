eb2a7c6a6ea2f9cc7db313e2094044a38439090e
Ryan Kohler <ryank@westwardleaning.com>
2017-04-12 13:10:15 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 letters = [
-  "A: alpha", "B: bravo", "C: charlie", "delta", "echo", 
+  "A: alpha", "B: bravo", "C: charlie", "D: delta", "echo", 
   "foxtrot", "golf", "hotel", "india", "juliet", 
   "kilo", "lima", "mike", "november", "oscar", 
   "papa", "quebec", "romeo", "sierra", "tango",
